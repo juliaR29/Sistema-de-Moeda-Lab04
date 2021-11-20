cf9eecd533b37b9a546ac61901c9da7fe10c3429
Júlia Prado <juliafpra@gmail.com>
2021-11-19 22:28:19 -0300

    Rotas CRUD empresa e Aluno

--- implementacao/routes/aluno.js
+++ implementacao/routes/aluno.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const pool = require("../lib/db");
 
+
+app.get("/cadastroAluno", (req, res) => {
+  res.render("cadastroAluno");
+});
+
 //create
 app.post("/api/aluno", async function (req, res) {
-  const { name, email, password } = req.body;
+  const { nome, email, cpf, rg, rua, complemento, bairro, instituicao, curso } = req.body;
 
   try {
-    await pool.query('INSERT INTO aluno (name, email, password) VALUES ($1, $2, $3)', [name, email, password]);
+    await pool.query('INSERT INTO aluno (nome, email, cpf, rg, rua, complemento, bairro, instituicao, curso) VALUES ($1, $2, $3)', [nome, email, cpf, rg, rua, complemento, bairro, instituicao, curso]);
     res.status(303)
       .header("location", "/login")
       .send("Cadastrado!");
@@ -19,15 +24,57 @@ app.post("/api/aluno", async function (req, res) {
 
 })
 
-app.get("/cadastroEmpresa", (req, res) => {
-    res.render("cadastroEmpresa");
-  });
-
-  
 //read
+app.get("/exibeAluno", async function (req, res)  {
+  const result = await pool.query('SELECT * FROM empresa');
+  res.render("exibeAluno", { alunos: result.rows });
+});
 
 //update
 
+app.get("/editarAluno/:id", async function (req, res) {
+  const id = req.params.id;
+  let result;
+  if (id) {
+      result = await pool.query('SELECT * FROM aluno WHERE id = $1', [id]);
+  } else {
+      res.status(404)
+          .send("Not Found");
+      return;
+  }
+
+  res.render("exibeAluno", { aluno: result.rows[0] });
+})
+
+app.post("/editarAluno/:id", checkAuth, async function (req, res) {
+  const { id } = req.params;
+  const { nome, email, cpf, rg, rua, complemento, bairro, instituicao, curso } = req.body;
+
+  try {
+      await pool.query('UPDATE aluno SET nome = $1, email = $2, cpf = $3, rua = $4, complemento = $5, bairro = $6, instituicao = $7, curso = $8 WHERE id = $7', [nome, email, cpf, rg, rua, complemento, bairro, instituicao, curso, id]);
+      res.status(303)
+          .header("location", "/exibeAluno")
+          .send("Atualizado!");
+  } catch (erro) {
+      res.status(500)
+          .send(erro.message);
+  }
+
+})
 //delete
+app.delete("/deleteAluno/:id", async function (req, res) {
+  const { id } = req.params;
+
+  try {
+      await pool.query('DELETE FROM aluno WHERE id = $1', [id]);
+      res.status(303)
+          .header("location", "/cadastroAluno")
+          .send("Deletado!");
+  } catch (erro) {
+      res.status(500)
+          .send(erro.message);
+  }
+
+})
   
 module.exports = app;
--- implementacao/routes/empresa.js
+++ implementacao/routes/empresa.js
@@ -3,6 +3,10 @@ const app = express();
 const pool = require("../lib/db");
 
 
+app.get("/cadastroEmpresa", (req, res) => {
+  res.render("cadastroEmpresa");
+});
+
 //create
 app.post("/api/empresa", async function (req, res) {
   const { name, email, password } = req.body;
@@ -20,15 +24,56 @@ app.post("/api/empresa", async function (req, res) {
 
 })
 
-app.get("/cadastroAluno", (req, res) => {
-    res.render("cadastroAluno");
-  });
-
-
 //read
+app.get("/exibeEmpresa", async function (req, res)  {
+  const result = await pool.query('SELECT * FROM empresa');
+  res.render("exibeEmpresa", { empresas: result.rows });
+});
 
 //update
+app.get("/editarEmpresa/:id", async function (req, res) {
+  const id = req.params.id;
+  let result;
+  if (id) {
+      result = await pool.query('SELECT * FROM empresa WHERE id = $1', [id]);
+  } else {
+      res.status(404)
+          .send("Not Found");
+      return;
+  }
+
+  res.render("exibeEmpresa", { empresa: result.rows[0] });
+})
+
+app.post("/editarEmpresa/:id", checkAuth, async function (req, res) {
+  const { id } = req.params;
+  const { nome, empresa, descricao, requisitos, beneficio, salario } = req.body;
+
+  try {
+      await pool.query('UPDATE empresa SET nome = $1, empresa = $2, descricao = $3, requisitos = $4, beneficio = $5, salario = $6 WHERE id = $7', [nome, empresa, descricao, requisitos, beneficio, salario, id]);
+      res.status(303)
+          .header("location", "/exibeAluno")
+          .send("Atualizado!");
+  } catch (erro) {
+      res.status(500)
+          .send(erro.message);
+  }
+
+})
 
 //delete
-  
+app.delete("/deleteEmpresa/:id", async function (req, res) {
+  const { id } = req.params;
+
+  try {
+      await pool.query('DELETE FROM empresa WHERE id = $1', [id]);
+      res.status(303)
+          .header("location", "/cadastroEmpresa")
+          .send("Deletado!");
+  } catch (erro) {
+      res.status(500)
+          .send(erro.message);
+  }
+
+})
 module.exports = app;
